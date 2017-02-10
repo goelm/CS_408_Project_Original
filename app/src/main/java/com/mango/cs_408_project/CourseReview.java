db57534fef77e043a1936f2a852f922283068c56
goelm <goelm@purdue.edu>
2017-02-09 19:15:16 -0500

    changed start screen to writing review

--- app/src/main/java/com/mango/cs_408_project/CourseReview.java
+++ app/src/main/java/com/mango/cs_408_project/CourseReview.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
 
 import com.facebook.CallbackManager;
 import com.facebook.login.widget.LoginButton;
@@ -28,7 +30,33 @@ public class CourseReview extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.add_info);
+
+        RadioButton rb_instructor = (RadioButton) findViewById(R.id.instructor);
+        //rb_instructor.setOnCheckedChangeListener(this);
+        RadioButton rb_ta = (RadioButton) findViewById(R.id.teachingassitant);
+        //rb_ta.setonRadioButtonClicked();
     }
+
+    //Course Review Stuff:
+    /*public void onRadioButtonClicked(android.view.View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.instructor:
+                if (checked){
+                    System.out.println("the instructor button was checked");
+                    RadioButton ta = (RadioButton)findViewById(R.id.teachingassitant);
+                    ta.setChecked(false);
+                }
+                break;
+            case R.id.teachingassitant:
+                if (checked)
+                    System.out.println("the teaching assistant button was checked");
+                break;
+        }
+    }*/
 
 }
--- app/src/main/java/com/mango/cs_408_project/MainActivity.java
+++ app/src/main/java/com/mango/cs_408_project/MainActivity.java
@@ -1,9 +1,12 @@
 package com.mango.cs_408_project;
 
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
 
 import com.facebook.CallbackManager;
 import com.facebook.login.widget.LoginButton;
@@ -12,6 +15,8 @@ import com.google.firebase.auth.FirebaseUser;
 
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+
+import static com.mango.cs_408_project.R.styleable.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -97,4 +102,5 @@ public class MainActivity extends AppCompatActivity {
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 */
+
 }
