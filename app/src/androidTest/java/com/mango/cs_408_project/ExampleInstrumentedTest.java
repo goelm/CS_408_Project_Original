811c90e3b0dbf66a3e7256120254bede670f782d
Kenny <zheng323@purdue.edu>
2017-02-10 21:11:42 -0500

    Instrumented Test

--- app/src/androidTest/java/com/mango/cs_408_project/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mango/cs_408_project/ExampleInstrumentedTest.java
@@ -1,9 +1,17 @@
 package com.mango.cs_408_project;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,6 +24,18 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+//    @Rule
+//    public ActivityTestRule<AddCourseReview> addCourseReviewTest =
+//            new ActivityTestRule<AddCourseReview>(AddCourseReview.class);
+
+//    @Test
+//    public void clickVeryAccessbleButton() throws Exception {
+//        onView(withId(R.id.add_course_ezAccess))
+//                .perform(click());
+//
+//    }
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
