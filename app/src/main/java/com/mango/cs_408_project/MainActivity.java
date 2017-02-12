dcb27ade47be4a0657e4dbbf344f4f5ba562feb3
Elvin Uthuppan <elvinutheman@gmail.com>
2017-02-11 21:17:40 -0500

    Sprint 1 Design Inspection Submission

--- app/src/main/java/com/mango/cs_408_project/MainActivity.java
+++ app/src/main/java/com/mango/cs_408_project/MainActivity.java
@@ -1,49 +1,12 @@
 package com.mango.cs_408_project;
-
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.Toast;
-
-import com.facebook.AccessToken;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FacebookAuthProvider;
-import com.google.firebase.auth.FirebaseAuth;
-import com.firebase.ui.auth.AuthUI;
-import com.google.firebase.auth.FirebaseUser;
-
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-
-import java.util.Arrays;
-
-import static com.mango.cs_408_project.R.styleable.View;
-
 
 public class MainActivity extends AppCompatActivity {
-
-
     // Initialize Facebook Login button
     /*mCallbackManager = CallbackManager.Factory.create();
     LoginButton loginButton = (LoginButton) findViewById(R.id.button_facebook_login);
     loginButton.setReadPermissions("email", "public_profile");*/
-    Button logOut = (Button) findViewById(R.id.logout_button);
-    FacebookLogin f = new FacebookLogin();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
