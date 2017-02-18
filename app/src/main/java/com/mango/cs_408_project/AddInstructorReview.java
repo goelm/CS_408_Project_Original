becbeba2d62d1ecc098acb1f637f805e4a01a42a
kendall-dev <bowlesk@purdue.edu>
2017-02-17 22:13:00 -0500

    a conflict didn't let me do the instructor review part, so here's that

--- app/src/main/java/com/mango/cs_408_project/AddInstructorReview.java
+++ app/src/main/java/com/mango/cs_408_project/AddInstructorReview.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RatingBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.util.regex.Matcher;
@@ -31,6 +32,9 @@ public class AddInstructorReview extends AppCompatActivity{
     int toughness; //1 for easy, 2 for mild, 3 for typical, 4 for tough, 5 for unreasonable
     boolean electronics; //true for allowed and false for not allowed
 
+    int value; //ranges from 0 to 100
+    int understand; //ranges from 0 to 100
+
     /* Button variables */
     RadioButton rb_instructor;
     RadioButton rb_ta;
@@ -46,6 +50,9 @@ public class AddInstructorReview extends AppCompatActivity{
     RadioButton rb_difTough;
     RadioButton rb_difCrazy;
     Button submit_button;
+
+    SeekBar seekV;
+    SeekBar seekU;
 
 
     @Override
@@ -69,6 +76,12 @@ public class AddInstructorReview extends AppCompatActivity{
         rb_difTough = (RadioButton) findViewById(R.id.diffButton4);
         rb_difCrazy = (RadioButton) findViewById(R.id.diffButton5);
         submit_button = (Button) findViewById(R.id.submitBut);
+
+        seekV = (SeekBar) findViewById(R.id.seekValue);
+        seekU = (SeekBar) findViewById(R.id.seekUnderstand);
+
+        final TextView textU = (TextView) findViewById(R.id.textUnderstand);
+        final TextView textV = (TextView) findViewById(R.id.textValue);
 
         /*
         RADIOBUTTON SWITCHING
@@ -200,6 +213,29 @@ public class AddInstructorReview extends AppCompatActivity{
          *************************************/
 
 
+        seekV.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+                value = progress;
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textV.setText(String.valueOf(value)+"%");
+            }
+        });
+
+        seekU.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+                understand = progress;
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textU.setText(String.valueOf(understand)+"%");
+            }
+        });
+
         /* When submitting the review */
         submit_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -242,6 +278,8 @@ public class AddInstructorReview extends AppCompatActivity{
                     review += "," + String.valueOf(extra_credit);
                     review += "," + Integer.toString(toughness);
                     review += "," + String.valueOf(electronics);
+                    review += "," + String.valueOf(value);
+                    review += "," + String.valueOf(understand);
                     s.write_instructor_review(first.getText().toString() + last.getText().toString(), review);
 
                 /* Go back to select a review */
