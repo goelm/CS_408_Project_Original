823ba21022867ebd172b95fe428429c3948b1539
Elvin Uthuppan <euthuppa@purdue.edu>
2017-03-02 00:56:20 -0500

    Added UI changes to instructor reviews and changed CustomAdapter to ProfCustomAdapter in ProfDisplay to get more accurate results.

--- app/src/main/java/com/mango/cs_408_project/ProfCustomAdapter.java
+++ app/src/main/java/com/mango/cs_408_project/ProfCustomAdapter.java
@@ -1,11 +1,11 @@
 package com.mango.cs_408_project;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -25,7 +25,6 @@ public class ProfCustomAdapter extends ArrayAdapter<ProfReview> implements View.
         TextView instructorName;
         RatingBar stars;
         TextView profComment;
-        ImageView info;
     }
 
     public ProfCustomAdapter(ArrayList<ProfReview> data, Context context) {
@@ -72,10 +71,9 @@ public class ProfCustomAdapter extends ArrayAdapter<ProfReview> implements View.
 
         //viewHolder.instructorName.setText(dataModel.instructorName);
         // TODO: maybe we could add a course section for each prof review like instructorName?
+        Log.d("DataModel rating: ", String.valueOf(dataModel.rating));
         viewHolder.stars.setRating(dataModel.rating);
         viewHolder.profComment.setText(dataModel.profComment);
-        viewHolder.info.setOnClickListener(this);
-        viewHolder.info.setTag(position);
         // Return the completed view to render on screen
         return convertView;
     }
