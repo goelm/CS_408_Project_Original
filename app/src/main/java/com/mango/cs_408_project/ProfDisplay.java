7a9d0eb061516ebf2f2b3102fff5a04e911ce9b2
goelm <goelm@purdue.edu>
2017-03-02 13:39:29 -0500

    more prof stats

--- app/src/main/java/com/mango/cs_408_project/ProfDisplay.java
+++ app/src/main/java/com/mango/cs_408_project/ProfDisplay.java
@@ -35,7 +35,7 @@ import java.util.Map;
 public class ProfDisplay extends AppCompatActivity {
     private ListView prof_mListView;
     private float rating = 0;
-    private int counter = 0;
+    private float counter = 0;
     private static ProfCustomAdapter prof_adapter;
     public static boolean has_user_input = true;
     String user_input;
@@ -43,10 +43,13 @@ public class ProfDisplay extends AppCompatActivity {
     TextView value_lectures_text;
     TextView understandable_text;
     TextView num_reviews_text;
+    TextView extra_credit_text;
     ProgressBar value_lectures_bar;
     ProgressBar understandable_bar;
+    ProgressBar extra_credit_bar;
     float value_lectures;
     float understandable;
+    int extra_credit;
 
 
     private final ArrayList<ProfReview> prof_reviews = new ArrayList<>();
@@ -125,27 +128,37 @@ public class ProfDisplay extends AppCompatActivity {
                     rating += instructor.rating;//for stars
                     value_lectures += instructor.seekV;
                     understandable +=instructor.seekU;
+                    if(instructor.extraCredit == true) {
+                        extra_credit++;
+                    }
                 }
                 //Set statistics here
                 stars.setRating(rating/prof_reviews.size());
                 prof_adapter.notifyDataSetChanged();
 
                 num_reviews_text = (TextView) findViewById(R.id.num_reviews_text);
-                num_reviews_text.setText(Integer.toString(counter)+" reviews");
+                num_reviews_text.setText(Integer.toString((int)counter)+" reviews");
 
                 value_lectures_text = (TextView) findViewById(R.id.value_lecture_heading);
                 understandable_text = (TextView) findViewById(R.id.understandable_text);
-                value_lectures_text.setText(value_lectures_text.getText()+" (" + Integer.toString((int)value_lectures/counter) + "%)");
-                understandable_text.setText(understandable_text.getText()+" (" + Integer.toString((int)understandable/counter) + "%)");
+                extra_credit_text = (TextView) findViewById(R.id.extra_credit_text);
+
+                value_lectures_text.setText(value_lectures_text.getText()+" (" + Integer.toString((int)(value_lectures/counter)) + "%)");
+                understandable_text.setText(understandable_text.getText()+" (" + Integer.toString((int)(understandable/counter)) + "%)");
+                extra_credit_text.setText(extra_credit_text.getText()+" (" + Integer.toString((int)((extra_credit/counter)*100)) + "%)");
 
                 value_lectures_bar = (ProgressBar) findViewById(R.id.value_lecture_bar);
                 understandable_bar = (ProgressBar) findViewById(R.id.understandable_bar);
+                extra_credit_bar = (ProgressBar) findViewById(R.id.extra_credit_bar);
 
                 value_lectures_bar.setMax(100);
                 understandable_bar.setMax(100);
+                extra_credit_bar.setMax(100);
 
                 value_lectures_bar.setProgress((int)(value_lectures/counter));
                 understandable_bar.setProgress((int)(understandable/counter));
+                Log.d("manasi",Integer.toString(extra_credit));
+                extra_credit_bar.setProgress((int)((extra_credit/counter)*100));
 
             }
 
